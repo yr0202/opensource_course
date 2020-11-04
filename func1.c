1db611d7cb827adf11d036c7d7ef4805559433fa
Nam Yuram <yooram0202@naver.com>
2020-11-03 21:55:38 -0800

    foo1{} is defined.

---
+++ func1.c
@@ -0,0 +1,7 @@
+#include <funcs.h>
+#include <stdio.h>
+
+void foo()
+{
+	printf("hello foo\n");
+}
--- include/funcs.h
+++ include/funcs.h
@@ -1,3 +1,6 @@
 #ifndef __FUNCH_H__
-#defint __FUNCH_H__
+#define __FUNCH_H__
 
+void foo();
+
+#endif
